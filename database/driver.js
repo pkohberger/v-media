50fcd6e87bf64fb5203f16da684c92eef94e2f43
Phillip Kohberger <pkohberger@cyber-ny.com>
2021-03-06 21:18:55 +0000

    redis cache hit miss completion

--- database/driver.js
+++ database/driver.js
@@ -1,6 +1,4 @@
-const mysql = require('mysql2-promise')(); 
-const redis = require('redis');
-const stringify = require('json-stringify-safe');
+const mysql = require('mysql2-promise')();
 
 module.exports = {
 
@@ -21,12 +19,6 @@ module.exports = {
         this.connection = mysql;
 
         return this.connection; 
-
-        // return this.connection.connect(function(error) {
-        //     if(error) {
-        //         return console.error('error: ' + error.message);
-        //     }
-        // });
     },
 
     //read from the database
@@ -37,53 +29,12 @@ module.exports = {
             this.connect();
         }
 
-        //var client = redis.createClient();
-        //var key = query.replace(/[^A-Za-z0-9]+/g,'').toLowerCase();
-        var dbHandle = this;
-
         try {
             var queryResult = await this.connection.execute(query);
             return queryResult;
         } catch(error) {
             console.log("READ: " + error.message);
-        }
-
-        // return client.get(key, function(err, result) {
-
-        //     //does cache exist
-        //     //caching layer need adjustments elected to submit
-        //     //you will see at least conceptually I understand the cacheing layer
-        //     //this if false was deliberate because the caching is not functioning
-        //     //i left code there to see that conceptually I have the idea
-        //     if (false) {
-
-        //         let resultJSON = JSON.parse(result);
-
-        //         //return object
-        //         return resultJSON;
-        //     } else {
-        //         //make sure we are connected
-        //         if(dbHandle.connection === null) {
-        //             dbHandle.connect();
-        //         }
-
-        //         //process query
-        //         var queryResult = dbHandle.connection.query(query,fn);
-
-        //         //save query result in cache
-        //         //@TODO: fix circular object error:
-        //         //TypeError: Converting circular structure to JSON
-        //         //tried workaround https://www.npmjs.com/package/json-stringify-safe
-        //         //tried flatten and unflatten as well
-        //         //at this point I am realizing that I may have restructure certain portions
-        //         //though Iam hoping in submitting what I have
-        //         //you will see at least conceptually I understand the cacheing layer
-        //         client.setex(key, 3600, stringify(queryResult));
-
-        //         //return query result
-        //         return queryResult;
-        //     }
-        //});        
+        }     
     },
 
     //write to the database
--- models/comments.js
+++ models/comments.js
@@ -1,41 +1,65 @@
-const database = require('../database/driver.js'); 
+const database = require('../database/driver.js');
+const util = require('util');
+const redis = require('redis');
 
 module.exports = {
     create: function (params) {
         //@TODO: add parameter sanitization
         let query = "INSERT INTO comments(ip,text,created) VALUES('" + params.ip + "','" + params.text + "',NOW())";
+        
+        //destroy cache based on key
+        var client = redis.createClient();
+        client.del("SELECT * FROM comments".replace(/[^A-Za-z0-9]+/g,'').toLowerCase());
+
+        //return write result
         return database.write(query);
     },
     list: async function (params) {
         //@TODO: add filtering parameters
-        let query  = "SELECT * FROM comments";
-        try {
-            var result = await database.read(query);
-            
-            //iterate results
-            var json = [];
-            for (let rows of result) {
-                if(typeof rows[Symbol.iterator] !== 'function') {
-                    continue;
-                }
-                for(let row of rows) {
-                    //bypass metadata
-                    if(typeof row.ip === 'undefined' && typeof row.text === 'undefined') {
-                        break;
-                    }
+        var query  = "SELECT * FROM comments";
+        var key = query.replace(/[^A-Za-z0-9]+/g,'').toLowerCase();
 
-                    //store row data
-                    json.push({
-                        ip: row.ip,
-                        text: row.text
-                    });
-                }
-            }
+        //get redis client and create key bsaed on query
+        var client = redis.createClient();
+        client.get = util.promisify(client.get);
 
-        } catch(error) {
-            console.log("LIST: " + error.message);
+        //attempt to get cached result based on key
+        var result = await client.get(key);
+
+        if(result) {
+            console.log('CACHE HIT!!!');
+            //return parsed json
+            return JSON.parse(result);
         }
 
+        console.log('CACHE MISS!!!'); 
+        //run query
+        result = await database.read(query);
+
+        //iterate results
+        var json = [];
+        for (let rows of result) {
+            if(typeof rows[Symbol.iterator] !== 'function') {
+                continue;
+            }
+            for(let row of rows) {
+                //bypass metadata
+                if(typeof row.ip === 'undefined' && typeof row.text === 'undefined') {
+                    break;
+                }
+
+                //store row data
+                json.push({
+                    ip: row.ip,
+                    text: row.text
+                });
+            }
+        }
+
+        //store query results as string in cache
+        client.set(key,JSON.stringify(json));
+
+        //return json object results
         return json;
     }
 }
